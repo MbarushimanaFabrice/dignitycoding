cc6938dafbb4c531293f1ef91b8b7bb33f5d230f
MBARUSHIMANA Fabrice <123445030+MbarushimanaFabrice@users.noreply.github.com>
2024-02-10 10:36:50 +0200

    Update main.jsx

--- src/Front_end/main.jsx
+++ src/Front_end/main.jsx
@@ -1,34 +1,15 @@
  import React from 'react';
  import App from './App.jsx';
 import ReactDOM from 'react-dom/client'
-import './Styles.css'
-import { BrowserRouter } from "react-router-dom"
-import global_kin from './cms/translations/Kin/global.json'
-import global_en from './cms/translations/en/global.json'
-import i18next from 'i18next'
-import { I18nextProvider } from 'react-i18next';
-i18next.init({
-  interpolation:{escapeValues:false},
-  lng:"en",
-  resources:{
-    en:{
-      global:global_en
-    },
-    kin:{
-      global:global_kin
-    }
-  }
-})
-
+ import { BrowserRouter } from "react-router-dom"
+   
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
       <BrowserRouter>
-      <I18nextProvider i18n={i18next}> 
-        <App />
-      </I18nextProvider>
-       
+         <App />
+        
       </BrowserRouter>
       
      </>
